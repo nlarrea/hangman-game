51920e576169387865d522ec4f177df766a8334b
nlarrea <larrea.naia@gmail.com>
2023-01-17 22:03:58 +0100

    go-back button added, start-game toggle js added

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // https://dictionaryapi.dev/                   -> obtener definicion de una palabra concreta
 // http://random-word-api.herokuapp.com/home    -> obtener la palabra para enviársela a la otra API
+const btnStart = document.querySelector("#start-game");
+const btnBack = document.querySelector("#back-game");
+const playingState = document.querySelectorAll(".not-playing");
+
+btnStart.addEventListener("click", () => {
+    playingState.forEach(element => {
+        element.classList.toggle("not-playing");
+        element.classList.toggle("playing");
+    })
+});
