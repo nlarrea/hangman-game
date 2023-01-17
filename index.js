7d4e018d5070a5f7fcd1d8782c7a28bc37a86f30
nlarrea <larrea.naia@gmail.com>
2023-01-17 15:12:06 +0100

    basic styles, container, header

---
+++ index.js
@@ -0,0 +1,2 @@
+// https://dictionaryapi.dev/                   -> obtener definicion de una palabra concreta
+// http://random-word-api.herokuapp.com/home    -> obtener la palabra para enviársela a la otra API
