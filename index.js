ebdcc9e7b8570245140a9a0ce442866efcbcb087
nlarrea <larrea.naia@gmail.com>
2023-01-18 01:37:08 +0100

    word fetch, gives a promise

--- index.js
+++ index.js
@@ -4,9 +4,31 @@ const btnStart = document.querySelector("#start-game");
 const btnBack = document.querySelector("#back-game");
 const playingState = document.querySelectorAll(".not-playing");
 
-btnStart.addEventListener("click", () => {
+function togglePlayingState(){
     playingState.forEach(element => {
         element.classList.toggle("not-playing");
         element.classList.toggle("playing");
     })
+}
+
+function getData(url){
+    return fetch(url)
+    .then(response => response.json());
+}
+
+function hangmanGame(){
+    const wordPromise = getData("https://random-word-api.herokuapp.com/word");
+    console.log(wordPromise.then());
+    
+    // const definitionPromise = getData();
+}
+
+btnStart.addEventListener("click", () => {
+    togglePlayingState();
+    hangmanGame();
 });
+
+btnBack.addEventListener("click", () => {
+    togglePlayingState();
+    // remove game data
+})
